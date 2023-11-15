97b7eed55e9775913b7a557ee23e70665bea749e
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2023-11-15 11:57:29 +0530

    Gravity Light Shadow Complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-
+import "./style.css";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
@@ -12,36 +12,92 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled=true
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+class Box extends THREE.Mesh {
+  constructor({
+    height,
+    width,
+    depth,
+    color = 0x00ff00,
+    velocity = { x: 0, y: 0, z: 0 },
+    position = { x: 0, y: 0, z: 0 },
+  }) {
+    super(
+      new THREE.BoxGeometry(width, height, depth),
+      new THREE.MeshStandardMaterial({ color })
+    );
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+    console.log(position.x, "x");
+    this.position.set(position.x, position.y, position.z);
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+    this.velocity = velocity;
+    this.gravity = -0.005;
+  }
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-cube.castShadow=true
+  update(group) {
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+    this.applyGravity()
+  }
+  applyGravity() {
+    this.velocity.y += this.gravity;
+
+    if (this.bottom + this.velocity.y <= ground.top) {
+      this.velocity.y *= 0.8;
+
+      this.velocity.y = -this.velocity.y;
+    } else this.position.y += this.velocity.y;
+  }
+}
+
+const cube = new Box({
+  width: 1,
+  height: 1,
+  depth: 1,
+  velocity: {
+    x: 0,
+    y: -0.01,
+    z: 0,
+  },
+});
+
+cube.castShadow = true;
 scene.add(cube);
 
-const ground = new THREE.Mesh(
-  new THREE.BoxGeometry(6, 0.5, 10),
-  new THREE.MeshStandardMaterial({ color: 0x0000ff })
-);
-ground.receiveShadow=true
-ground.position.y = -2;
+const ground = new Box({
+  width: 5,
+  height: 0.5,
+  depth: 10,
+  color: 0x0000ff,
+  position: {
+    x: 0,
+    y: -2,
+    z: 0,
+  },
+});
+ground.receiveShadow = true;
 scene.add(ground);
 
 //Light
-const light=new THREE.DirectionalLight(0xffffff,1)
-light.castShadow=true
-scene.add(light)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.z = 2;
+light.position.y = 3;
+light.castShadow = true;
+scene.add(light);
 
 camera.position.z = 5;
+console.log(ground.top);
+console.log(cube.bottom);
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
+  cube.update(ground);
 }
 animate();
